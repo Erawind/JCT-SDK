5f1ad8fae5aed59ebfd1260574bfcad708d6ad75
Erawind <myerawind@gmail.com>
2026-01-18 07:08:25 +0800

    Update jct_wrapper.py

--- jct_wrapper.py
+++ jct_wrapper.py
@@ -7,12 +7,11 @@ lib_path = "./jct_engine.dll" if platform.system() == "Windows" else "./jct_engi
 
 try:
     _lib = ctypes.CDLL(lib_path)
-    # 這裡必須對應你剛才那份 C 代碼的函數名稱：jct_core_process
     _lib.jct_core_process.argtypes = [ctypes.c_int, ctypes.c_double, ctypes.c_double]
     _lib.jct_core_process.restype = ctypes.c_double
 
     def correct_acceleration(acc_newton):
-        # 執行你 code 裡的 mode 0
+        # 執行 mode 0
         return _lib.jct_core_process(0, acc_newton, 0.0)
 
 except OSError:
